4ae6657794842864f735bd15ed6fdcffaf17cd99
duamkr <duamkr@gmail.com>
2019-07-02 23:55:03 +0900

    ....

--- 01_Crawling/오진영_Crawling.R
+++ 01_Crawling/오진영_Crawling.R
@@ -1,18 +1,23 @@
-require(rvest)
+install.packages("rvest")
+library(dplyr)
+library(stringr)
+library(openxlsx)
+library(xlsx)
+
 
 trim <- function(x) gsub("^\\s+|\\s+$", "", x)
-cat_names = c('슈퍼시드피자', '프리미엄', '클래식')
-categories = c('C0101', 'C0102', 'C0104')
-page = c(1,2,4)
+cat_names <- c('슈퍼시드피자', '프리미엄', '클래식')
+categories <- c('C010', 'C010', 'C010')
+
 base_url <- 'https://web.dominos.co.kr/goods/list?dsp_ctgr='
 
 
 wb <- createWorkbook()
-for (i in 1:3) {
- df_pizza <- data.frame(title=c(), L_pirce=c(), M_price=c())
-    url1 <- paste0(base_url,categories)
+  for (i in 1:3) {
+    df_pizza <- data.frame(title=c(), L_pirce=c(), M_price=c())
+    url1 <- paste0(base_url,categories,i)
     print(url1)
-} {
+
     read_html(url1) %>%
       html_node('.lst_prd_type') %>%
       html_nodes('li') -> lis
@@ -32,8 +37,5 @@ for (i in 1:3) {
  pizza <- data.frame(title=title, L_price=L_price, M_price=M_price)
  df_pizza <- rbind.data.frame(df_pizza, pizza)
 }
-  addWorksheet(wb, cat_names)
-  writeDataTable(wb, cat_names, df_pizza)
-} 
 saveWorkbook(wb, file="D:/workspace/R_Project/01_Crawling/domino_pizza.xlsx")
 repair_encoding(title, from = 'utf-8q')
