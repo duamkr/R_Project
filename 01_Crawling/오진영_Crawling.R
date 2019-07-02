669784dcf166cb7b0f5fa6be476647af45f8292a
duamkr <duamkr@gmail.com>
2019-07-02 17:52:43 +0900

    first commit

---
+++ 01_Crawling/오진영_Crawling.R
@@ -0,0 +1,39 @@
+require(rvest)
+
+trim <- function(x) gsub("^\\s+|\\s+$", "", x)
+cat_names = c('슈퍼시드피자', '프리미엄', '클래식')
+categories = c('C0101', 'C0102', 'C0104')
+page = c(1,2,4)
+base_url <- 'https://web.dominos.co.kr/goods/list?dsp_ctgr='
+
+
+wb <- createWorkbook()
+for (i in 1:3) {
+ df_pizza <- data.frame(title=c(), L_pirce=c(), M_price=c())
+    url1 <- paste0(base_url,categories)
+    print(url1)
+} {
+    read_html(url1) %>%
+      html_node('.lst_prd_type') %>%
+      html_nodes('li') -> lis
+    lis
+    
+    L_price <- c()
+    M_price <- c()
+    title <- c()
+    for (li in lis) {
+      pr <- html_node(li, '.price_large') %>% html_text()
+      pr <- gsub("\\\\", "", pr)
+      L_price <- c(L_price,pr)
+      #L_price <- c(L_price, html_node(li, '.price_large') %>% html_text())
+      M_price <- c(M_price, html_node(li, '.price_medium') %>% html_text())
+      title <- c(title, html_node(li, '.prd_title') %>% html_text())
+    }
+ pizza <- data.frame(title=title, L_price=L_price, M_price=M_price)
+ df_pizza <- rbind.data.frame(df_pizza, pizza)
+}
+  addWorksheet(wb, cat_names)
+  writeDataTable(wb, cat_names, df_pizza)
+} 
+saveWorkbook(wb, file="D:/workspace/R_Project/01_Crawling/domino_pizza.xlsx")
+repair_encoding(title, from = 'utf-8q')
